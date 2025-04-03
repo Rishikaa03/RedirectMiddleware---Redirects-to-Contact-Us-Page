d1f1837318c7e2ec0b79b3030e077c2e6780a41f
Rishika <Rishika@RISHIKA>
2025-04-03 09:58:50 +0530

    Add project files.

---
+++ RedirectMiddleware/Controllers/MiddleWare/ProductRedirectMiddleware.cs
@@ -0,0 +1,19 @@
+﻿namespace RedirectMiddleware.Controllers.MiddleWare
+{
+    public class ProductRedirectMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ProductRedirectMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if(context.Request.Query.ContainsKey("productId") && context.Request.Query["productId"] == "21")
+            {
+                context.Response.Redirect("contactUs");
+            }
+            await _next(context);
+        }
+    }
+}
---
+++ RedirectMiddleware/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RedirectMiddleware.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
